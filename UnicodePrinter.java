585e3245e007d4a67a5b5613befadca8b7ea7c35
Amaan Marfatia <amaanmarfatia@gmail.com>
2019-02-02 13:41:03 -0600

    Change strings to characters

--- UnicodePrinter.java
+++ UnicodePrinter.java
@@ -13,29 +13,29 @@ public class UnicodePrinter extends BoardPrinter {
             StringBuilder sb = new StringBuilder();
             for (int x = 0; x < arr.length; x++) {
                 sb.append(getUnicode(arr[x][y]));
-                sb.append(" ");
+                sb.append(' ');
 
             }
             System.out.println(sb);
         }
     }
 
-    private String getUnicode(char c) {
+    private char getUnicode(char c) {
         switch (c) {
-            case 'p': return "♟";
-            case 'n': return "♞";
-            case 'b': return "♝";
-            case 'r': return "♜";
-            case 'q': return "♛";
-            case 'k': return "♚";
-            case 'P': return "♙";
-            case 'N': return "♘";
-            case 'B': return "♗";
-            case 'R': return "♖";
-            case 'Q': return "♕";
-            case 'K': return "♔";
+            case 'p': return '♟';
+            case 'n': return '♞';
+            case 'b': return '♝';
+            case 'r': return '♜';
+            case 'q': return '♛';
+            case 'k': return '♚';
+            case 'P': return '♙';
+            case 'N': return '♘';
+            case 'B': return '♗';
+            case 'R': return '♖';
+            case 'Q': return '♕';
+            case 'K': return '♔';
         }
-        return " ";
+        return ' ';
     }
 
 }
